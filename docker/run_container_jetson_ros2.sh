7b007f354f9686870bc7be655b092f7ed6893b99
s-jann <sjank964@gmail.com>
2026-05-23 22:22:41 +0900

    Add RealSense support to ROS2 Jetson container

--- docker/run_container_jetson_ros2.sh
+++ docker/run_container_jetson_ros2.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
-# Run FoundationPose container on Jetson AGX Orin
-# Uses --runtime nvidia (L4T convention) instead of --gpus all
+# Run FoundationPose ROS2 container on Jetson AGX Orin
+# JetPack 6 / ROS2 Humble version
 
-docker rm -f foundationpose-jetson >/dev/null 2>&1
+docker rm -f foundationpose-jetson-yolo >/dev/null 2>&1
 DIR=$(cd "$(dirname "$0")/.." && pwd)
 
 xhost + >/dev/null 2>&1
+
 docker run \
     --runtime nvidia \
     --env NVIDIA_DISABLE_REQUIRE=1 \
     -it \
+    --privileged \
     --network=host \
     --name foundationpose-jetson-yolo \
     --ipc=host \
     --cap-add=SYS_PTRACE \
     --security-opt seccomp=unconfined \
+    -v /dev:/dev \
     -v "$DIR":"$DIR" \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     -v /tmp:/tmp \
-    --ipc=host \
     -e DISPLAY=${DISPLAY} \
-    -e ROS_MASTER_URI=http://localhost:11311 \
-    -e ROS_IP=127.0.0.1 \
     -e GIT_INDEX_FILE \
     foundationpose-jetson-yolo:ros2-humble \
     bash -c "cd $DIR && bash"
